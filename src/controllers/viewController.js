9c82a62df5db9c41ec55d1186ab1b24e735eb3f1
Uchechukwu Udo <udoigweuchechukwu@gmail.com>
2025-03-03 16:15:10 +0100

    chore: view controller; added store abd user view controllers

--- src/controllers/viewController.js
+++ src/controllers/viewController.js
@@ -1,19 +1,29 @@
 module.exports = {
-	home: async (req, res) => {
-		res.render("index", { title: "IStock - Home" });
-	},
-	login: async (req, res) => {
-		res.render("login", { title: "IStock - Sign In" });
-	},
+  home: async (req, res) => {
+    res.render("index", { title: "IStock - Home" });
+  },
+  login: async (req, res) => {
+    res.render("login", { title: "IStock - Sign In" });
+  },
 
-	/* SUPER ADMIN */
-	saHome: async (req, res) => {
-		res.render("sa/dashboard", {
-			title: "iStock - Dashboard",
-		});
-	},
+  /* SUPER ADMIN */
+  saHome: async (req, res) => {
+    res.render("sa/dashboard", {
+      title: "iStock - Dashboard",
+    });
+  },
+  saStores: async (req, res) => {
+    res.render("sa/stores", {
+      title: "iStock - Stores",
+    });
+  },
+  saUsers: async (req, res) => {
+    res.render("sa/users", {
+      title: "iStock - Users",
+    });
+  },
 
-	/* ADMIN */
+  /* ADMIN */
 
-	/* CASHIER */
+  /* CASHIER */
 };
